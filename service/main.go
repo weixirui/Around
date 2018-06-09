ca993e461c6dad7005266f2fbd6b16006b5c4abc
weixirui <q576077015@gmail.com>
2018-06-09 08:35:21 -0700

    service update

--- service/main.go
+++ service/main.go
@@ -1,71 +1,169 @@
 package main
+
 import (
-	"fmt"
 	"encoding/json"
-	"net/http"
+	"fmt"
+	"github.com/pborman/uuid"
+	elastic "gopkg.in/olivere/elastic.v3"
 	"log"
+	"net/http"
+	"reflect"
 	"strconv"
 )
 
-type Location struct{
+type Location struct {
 	Lat float64 `json:"lat"`
 	Lon float64 `json:"lon"`
 }
 
-type Post struct{
-	User     string `json:"user"`
-	Message  string `json:"message"`
+type Post struct {
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
 	Location Location `json:"location"`
 }
 
-func main(){
+func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+			"mappings":{
+				"post":{
+					"properties":{
+						"location":{
+							"type":"geo_point"
+						}
+					}
+				}
+			}
+		}`
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
+
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request){
+func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one post request.")
 	decoder := json.NewDecoder(r.Body)
 	var p Post
-	if err := decoder.Decode(&p); err !=nil{
+	if err := decoder.Decode(&p); err != nil {
 		panic(err)
 	}
-	
+
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	id := uuid.New()
+	saveToES(&p, id)
 
 }
 
-const(
-	DISTANCE="200km"
-)
-
-func handlerSearch(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Received one request for search")
-	lat,_ := strconv.ParseFloat(r.URL.Query().Get("lat"),64)
-	lon,_ := strconv.ParseFloat(r.URL.Query().Get("lon"),64)
-	ran := DISTANCE
-	if val :=r.URL.Query().Get("range"); val !=""{
-		ran=val+"km"
-	}
-	fmt.Println("range is ", ran)	
-	p :=&Post{
-		User:"1111",
-		Message:"sfsfa",
-		Location: Location{
-			Lat:lat,
-			Lon:lon,
-		},
-	}
-	
-	js, err :=json.Marshal(p)
-	if err != nil{
+func saveToES(p *Post, id string) {
+	// Create a client
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
 		panic(err)
 		return
 	}
-	
-	w.Header().Set("Conten-Type", "application/json")
+
+	// Save it to index
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+}
+
+const (
+	INDEX    = "around"
+	TYPE     = "post"
+	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.184.5.49:9200"
+)
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+	fmt.Println("Seach received: %f %f %s\n", lat, lon, ran)
+
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+
+	if err != nil {
+		panic(err)
+	}
+
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+		p := item.(Post) // p = (Post) item
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
-	
+
 }
